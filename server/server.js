ed3300eb1a151895fca06b5f40feaa13797495ce
jpdhernandez <jpdhernandez@gmail.com>
2017-02-26 23:55:23 -0500

    Add a way to emit and listen to events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New user connected");
 
+  socket.emit("newEmail", {
+    from: "julian@example.com",
+    text: "Hey, this an email!",
+    createdAt: 260217
+  });
+
+  socket.on("createEmail", (newEmail) => {
+    console.log("createEmail", newEmail);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
