9e28d592e6615530201ec89f5024ac9b54019393
jpdhernandez <jpdhernandez@gmail.com>
2017-03-11 21:34:14 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on("connect", function() {
   console.log("connected to server");
-
-  socket.emit("createMessage", {
-    from: "Julian",
-    text: "Yup, that's cool"
-  });
 });
 
 socket.on("disconnect", function() {
